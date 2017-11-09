835486755b71bfe692f4082dfe2caebdb0bcf115
Ming Lei <ming.lei@redhat.com>
2017-11-09 10:49:53 -0800

    block: Make q_usage_counter also track legacy requests
    This patch makes it possible to pause request allocation for
    the legacy block layer by calling blk_mq_freeze_queue() and
    blk_mq_unfreeze_queue().
    
    Change-Id: Id6d86a2e73f01043349969bd5df99e10c8a6a092
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Tested-by: Martin Steigerwald <martin@lichtvoll.de>
    Tested-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Cc: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Git-commit: 055f6e18e08f5b7fd98171fce857a0bad87a919d
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Pradeep P V K <ppvk@codeaurora.org>
    (cherry picked from commit f71ff9fdcc0547f018657bef3592ab60af1ca768)

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -146,7 +146,6 @@ bool blk_freeze_queue_start(struct request_queue *q)
 		percpu_ref_kill(&q->q_usage_counter);
 		if (q->mq_ops)
 			blk_mq_run_hw_queues(q, false);
-		return true;
 	}
 	return false;
 }
@@ -280,13 +279,6 @@ void blk_mq_wake_waiters(struct request_queue *q)
 	queue_for_each_hw_ctx(q, hctx, i)
 		if (blk_mq_hw_queue_mapped(hctx))
 			blk_mq_tag_wakeup_all(hctx->tags, true);
-
-	/*
-	 * If we are called because the queue has now been marked as
-	 * dying, we need to ensure that processes currently waiting on
-	 * the queue are notified as well.
-	 */
-	wake_up_all(&q->mq_freeze_wq);
 }
 
 bool blk_mq_can_queue(struct blk_mq_hw_ctx *hctx)
