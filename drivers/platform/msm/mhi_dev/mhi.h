4fa1f331f7e8d64d082d7a0bcbf5743e7bad037e
Siddartha Mohanadoss <smohanad@codeaurora.org>
2020-05-01 10:34:21 -0700

    msm: mhi_dev: Fix maximum number of HW channels
    Update maximum number of hardware channels supported by
    the device to support QMAP flow control and QDSS HW
    channels.
    
    Change-Id: I6d20c5a6f1b2a58ff18aab660a827fdec95e0b7a
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>
    (cherry picked from commit 27a8cbdd4682dd3dbb786a2a8b939a4a908f00e8)
    (cherry picked from commit d70140f4a9d7ce1f57eaeb7742404ad667596258)

--- drivers/platform/msm/mhi_dev/mhi.h
+++ drivers/platform/msm/mhi_dev/mhi.h
@@ -270,7 +270,6 @@ struct mhi_config {
 
 #define NUM_CHANNELS			128
 #define HW_CHANNEL_BASE			100
-#define NUM_HW_CHANNELS			15
 #define HW_CHANNEL_END			110
 #define MHI_ENV_VALUE			2
 #define MHI_MASK_ROWS_CH_EV_DB		4
