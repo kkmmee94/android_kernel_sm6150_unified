5082f73f0c53170abf114849c36bc78834cc94a5
firemax13 <mydadisbrave2@gmail.com>
2020-09-03 14:59:22 +0800

    fire.sh: Fix up some compile spamming
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- fire.sh
+++ fire.sh
@@ -6,19 +6,22 @@ set -e
 # No need to change the whole and entire file to set-up kernel path
 KERNEL_PATH=/home/firemax13/r1qkernel
 
+# Export GCC and ARCH
 export CROSS_COMPILE=$KERNEL_PATH/toolchain/aarch64-linux/bin/aarch64-linux-android-
 export ARCH=arm64
 
+# Output hacking & tricking
 if [ ! -d out ]; then
 	mkdir out
 fi
 
+# Types, paths, and more etc.
 BUILD_CROSS_COMPILE=$KERNEL_PATH/toolchain/aarch64-linux/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=$KERNEL_PATH/toolchain/clang/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE fire_defconfig
-make -j64 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+make -C $KERNEL_PATH O=$KERNEL_PATH/out ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE fire_defconfig
+make -j4 -C $KERNEL_PATH O=$KERNEL_PATH/out ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
 
 tools/mkdtimg create out/arch/arm64/boot/dtbo.img --page_size=4096 $(find out -name "*.dtbo")
