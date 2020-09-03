5b6950405ff8ba1e26a203dec7b91860dcd5d889
firemax13 <mydadisbrave2@gmail.com>
2020-09-03 10:06:18 +0800

    Revert "msm_performance: simplify touchboost check and process"
    This reverts commit c03588f4a46767e2fc8d52610351e4f79ac79f0e.

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -62,9 +62,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 
-	if (touchboost == 0)
-		return 0;
-
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -73,7 +70,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 		return -EINVAL;
 
 	cp = buf;
-
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
@@ -142,9 +138,6 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 
-	if (touchboost == 0)
-		return 0;
-
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -153,7 +146,6 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 		return -EINVAL;
 
 	cp = buf;
-
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
